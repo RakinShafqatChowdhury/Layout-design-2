62dd6ea656ef1ef7fcbe46ec23e12703d2f3ba2c
RakinShafqatChowdhury <rakinshafqat@gmail.com>
2020-09-21 13:32:32 +0600

    layout design to show information

---
+++ app/src/main/java/com/paperfly/layoutdesign2/PaymentInformation.java
@@ -0,0 +1,14 @@
+package com.paperfly.layoutdesign2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_information);
+    }
+}
