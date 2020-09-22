b32d1a9caf981749d2bb73f69156273d45444f61
RakinShafqatChowdhury <rakinshafqat@gmail.com>
2020-09-22 11:54:24 +0600

    layout updated

--- app/src/main/java/com/paperfly/layoutdesign2/MainActivity.java
+++ app/src/main/java/com/paperfly/layoutdesign2/MainActivity.java
@@ -2,7 +2,10 @@ package com.paperfly.layoutdesign2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView logo = findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,PaymentInformation.class));
+            }
+        });
     }
 }
